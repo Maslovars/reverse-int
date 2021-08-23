d8526a64821d289f852263e48f739264eb57e053
Maslovars <maslovars@gmail.com>
2021-08-23 23:33:15 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const reverseN = parseFloat(n.toString().split('').reverse().join(''));
+    return reverseN;
 }
